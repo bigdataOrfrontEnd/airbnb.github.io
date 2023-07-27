2f9e1389281852fbd34c9cebf8f46dcd331dcb26
17639638825 <562985990@qq.com>
2023-07-27 10:19:28 +0800

    HeaderWrapper布局

--- src/components/app-header/index.jsx
+++ src/components/app-header/index.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import HeaderCenter from "./c-cpns/header-center";
 import HeaderLeft from "./c-cpns/header-left";
 import HeaderRifht from "./c-cpns/header-right";
-import { HeaderWrapper } from "./style";
+
+import styled from "styled-components";
 function AppHeader() {
+  // 样式
+  const HeaderWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    height: 80px;
+    border-bottom: 1px solid #eee;
+  `;
   return (
     <HeaderWrapper>
       <HeaderLeft />
