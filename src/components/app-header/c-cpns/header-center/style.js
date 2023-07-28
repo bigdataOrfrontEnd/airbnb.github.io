18e9ca6cfc48bcece8c71d90e484a397c82b39cd
17639638825 <562985990@qq.com>
2023-07-28 22:13:04 +0800

    center

--- src/components/app-header/c-cpns/header-center/style.js
+++ src/components/app-header/c-cpns/header-center/style.js
@@ -1,3 +1,31 @@
 import { styled } from "styled-components";
 
-export const CenterWrapper = styled.div``;
+export const CenterWrapper = styled.div`
+  width: 400px;
+  height: 50px;
+  border-radius: 40px;
+  border: 1px solid #ddd;
+  color: ${(props) => props.theme.text.secondaryColor};
+  font-weight: 800;
+  .search-bar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 50px;
+    width: 400px;
+    .text {
+      padding-left: 20px;
+    }
+    .icon {
+      display: flex;
+      align-items: center;
+      width: 32px;
+      height: 32px;
+      border-radius: 50%;
+      background-color: ${(props) => props.theme.color.primaryColor};
+      margin-right: 20px;
+      justify-content: center;
+      color: #fff;
+    }
+  }
+`;
