6bfecf942b84018e37935ac5ff87e076e5abd701
17639638825 <562985990@qq.com>
2023-08-09 17:03:35 +0800

    平分可交互

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import { Rate } from "antd";
 import { RoomWrapper } from "./style";
 
 const Room = memo((props) => {
   const { roomData } = props;
+  const [rat, setRat] = useState();
+  function getRatData(e) {
+    setRat(e);
+  }
   return (
     <RoomWrapper color={roomData.verify_info.text_color}>
       <div className="item" key={roomData.id}>
@@ -22,11 +26,21 @@ const Room = memo((props) => {
         <div className="price">{roomData.price_format}/晚</div>
         <div className="bootom">
           <Rate
-            allowHalf
             defaultValue={roomData.star_rating}
             style={{ color: roomData.star_rating_color }}
+            value={rat}
+            onHoverChange={(e) => getRatData(e)}
           />
-          {roomData.reviews_count}*{roomData?.bottom_info?.content}
+          <span>{roomData.reviews_count}</span>
+          <span
+            style={{
+              color: roomData?.bottom_info?.content_color,
+              fontSize: roomData?.bottom_info?.font_size,
+            }}
+          >
+            {roomData?.bottom_info?.content &&
+              "*" + roomData.bottom_info.content}
+          </span>
         </div>
       </div>
     </RoomWrapper>
