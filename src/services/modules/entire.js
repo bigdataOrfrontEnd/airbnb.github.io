db3befc4c5466f44e2ed2f55c6a44d91f1871b35
17639638825 <562985990@qq.com>
2023-08-18 17:15:57 +0800

    entire接口

---
+++ src/services/modules/entire.js
@@ -0,0 +1,10 @@
+import hyRequest from "../request";
+export function getEntireRoomList(offset, size = 20) {
+  return hyRequest.get({
+    url: "/entire/list",
+    params: {
+      offset,
+      size,
+    },
+  });
+}
--- src/views/entire/index.jsx
+++ src/views/entire/index.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import { getEntireRoomList } from "@/services/modules/entire";
+import React, { useEffect } from "react";
 
 function Entire() {
+  useEffect(() => {
+    getEntireRoomList(0).then((res) => {
+      console.log(res);
+    }, []);
+  });
   return <div>Entire</div>;
 }
 
